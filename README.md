037420169a152a162da6667c6c6ef5fb7e95b63a
neeekou <neeekou@gmail.com>
2021-04-22 22:10:34 +0200

    Added shared catalog
    Mounted to /repos
    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dev-ops-sandbox
+
+# Must have installed:
+# Vagrant
+# VirtualBox
+
+
+# vagrant commands
+vagrant plugin install vagrant-vbguest
+vagrant up
+vagrant ssh
--- vagrant/provide.sh
+++ vagrant/provide.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 sudo yum -y update
+sudo yum -y install vim
 sudo yum -y install epel-release
 sudo yum -y update
 sudo yum -y install ansible
+
+## Ansible magic
