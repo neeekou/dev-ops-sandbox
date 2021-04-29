39a694d8ef70b03390735014a1f50fe5d814fa67
neeekou <neeekou@gmail.com>
2021-04-29 18:34:59 +0200

    Managed to set up Django conf

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ VirtualBox
 # vagrant commands
 vagrant plugin install vagrant-vbguest
 vagrant up
+//At this point mount will not work
+vagrant ssh -c 'sudo yum -y update'
+vagrant ssh -c 'sudo shutdown now'
+vagrant up
 vagrant ssh
--- vagrant/provide.sh
+++ vagrant/provide.sh
@@ -6,4 +6,13 @@ sudo yum -y install epel-release
 sudo yum -y update
 sudo yum -y install ansible
 
+
 ## Ansible magic
+ansible-playbook /repos/dev-ops-sandbox/ansible/install-tools.yml
+ansible-playbook /repos/dev-ops-sandbox/ansible/configure-database.yml
+
+sudo pip3 install --upgrade django
+
+cd /repos/Projekt_studia/projekt/src/baza
+python3 manage.py migrate
+python3 manage.py runserver 192.168.33.10:8000
